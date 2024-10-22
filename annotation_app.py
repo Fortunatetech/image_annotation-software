adef6261a8e7fb4a8cf21a70a1b249d360669143
Ayodele Ayodeji <ayodeleayodeji250@gmail.com>
2024-10-22 16:36:39 +0100

    Update annotation_app.py

--- annotation_app.py
+++ annotation_app.py
@@ -13,7 +13,7 @@ load_dotenv()
 # Google API Configuration
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel("models/gemini-1.5-pro-002")
+model = genai.GenerativeModel("models/gemini-1.5-flash-001")
 
 # Function to generate image descriptions using LLM
 def generate_image_descriptions(image, prompt):
