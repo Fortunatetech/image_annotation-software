83d5e93aa27da0489995c3eb0c459fa8d2cc68e5
Ayodele Ayodeji <ayodeleayodeji250@gmail.com>
2024-10-31 12:23:08 +0100

    Update main_software.py

--- main_software.py
+++ main_software.py
@@ -12,7 +12,7 @@ load_dotenv()
 # Google API Configuration
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel("models/gemini-1.5-flash-8b")
+model = genai.GenerativeModel("models/gemini-1.5-flash-002")
 
 # Predefined password
 APP_PASSWORD = os.getenv("APP_PASSWORD")  # Load password from environment variables
